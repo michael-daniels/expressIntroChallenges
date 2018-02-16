2df4991d5b3c18ca0a085746b55fbef69b441529
‘Michael <michael.daniels.jr@gmail.com>
2018-02-16 11:39:19 -0700

    part two update

--- part2/server.js
+++ part2/server.js
@@ -1,9 +1,35 @@
-var express = require('express');
-var app = express();
-var port = process.env.PORT || 8000;
+let fs = require('fs');
+let express = require('express');
+let app = express();
+let bodyParser = require('body-parser');
+let port = process.env.PORT || 8000;
 
-app.get('/yourroute', function(req, res) {
-  res.send("stuff");
+app.get('/', function(req, res) {
+  let theData = fs.readFileSync('storage.json', "utf8");
+  res.send(theData);
+});
+
+app.get('/:name', function(req, res) {
+  let theName = fs.readFileSync('storage.json', "utf8");
+  let theJsonData = JSON.parse(theName);
+  if (theJsonData.name === req.params.name) {
+    res.send(theJsonData);
+  } else {
+    res.sendStatus(404)
+  }
+});
+
+app.post('/create/:name/:age', function(req, res) {
+  let theObj = {
+    name:`${req.params.name}`,
+    age:`${req.params.age}`
+  }
+  fs.writeFile('storage.json', JSON.stringify(theObj), (err) => {
+    if (err) {
+      throw err;
+    }
+  });
+  res.sendStatus(200);
 });
 
 app.use(function(req, res) {
