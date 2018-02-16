bc991ef1b8df95587417af4e3c247967abbd883a
‘Michael <michael.daniels.jr@gmail.com>
2018-02-16 16:25:03 -0700

    stretch goal complete

--- part2/server.js
+++ part2/server.js
@@ -6,30 +6,51 @@ let port = process.env.PORT || 8000;
 
 app.get('/', function(req, res) {
   let theData = fs.readFileSync('storage.json', "utf8");
-  res.send(theData);
+  let theJsonData = JSON.parse(theData);
+  res.json(theJsonData);
 });
 
-app.get('/:name', function(req, res) {
+app.get('/:id', function(req, res) {
   let theName = fs.readFileSync('storage.json', "utf8");
   let theJsonData = JSON.parse(theName);
-  if (theJsonData.name === req.params.name) {
-    res.send(theJsonData);
-  } else {
-    res.sendStatus(404)
+  for (let i = 0; i < theJsonData.length; i++) {
+    if (theJsonData[i].id === req.params.id) {
+      res.json(theJsonData[i]);
+      return;
+    };
   }
+  res.sendStatus(400);
+
 });
 
 app.post('/create/:name/:age', function(req, res) {
-  let theObj = {
-    name:`${req.params.name}`,
-    age:`${req.params.age}`
-  }
-  fs.writeFile('storage.json', JSON.stringify(theObj), (err) => {
-    if (err) {
-      throw err;
-    }
+
+  fs.readFile("counter.json", "utf8", (err, data) => {
+    console.log(data);
+    theCounter = JSON.parse(data);
+    theCounter++;
+    console.log(theCounter);
+
+    fs.writeFileSync("counter.json", JSON.stringify(theCounter));
+    console.log("wrote it");
   });
-  res.sendStatus(200);
+
+  let theCount = fs.readFileSync("counter.json", "utf8");
+
+  let theObj = {id: `${theCount}`, name:`${req.params.name}`, age:`${req.params.age}`};
+
+  fs.readFile("storage.json", "utf8", (err, data) => {
+    console.log(data);
+    let theArray = JSON.parse(data);
+    theArray.push(theObj);
+    console.log(theArray);
+
+
+    fs.writeFileSync("storage.json", JSON.stringify(theArray));
+    console.log("wrote it");
+    res.sendStatus(200);
+  })
+
 });
 
 app.use(function(req, res) {
